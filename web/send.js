3b7fc64791ec59b8398cd4e4c1fdc96177175842
i5ting <i5ting@126.com>
2016-05-01 22:15:55 +0800

    add default / to index.html

--- web/send.js
+++ web/send.js
@@ -2,9 +2,6 @@ var send = require('koa-send');
 var Koa = require('koa');
 var app = new Koa();
 
-// $ GET /package.json
-// $ GET /
-
 app.use(function (ctx, next){
   if ('/' == ctx.path) ctx.path = 'index.html';
   return send(ctx, ctx.path, { root: __dirname + '/starter-template' });
