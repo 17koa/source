a8badc8b7e8bdcb1d7ff101dd5b0154d04560757
i5ting <i5ting@126.com>
2016-06-22 00:25:30 +0800

    lazycommmit, please ignore

--- async/promise/reflow/index.js
+++ async/promise/reflow/index.js
@@ -3,3 +3,7 @@ var tasks = require('./tasks')
 tasks.hello('./package.json').then(tasks.step1).then(tasks.step2).catch(function(err) {
   console.log(err)
 })
+
+// tasks.hello('./package.json').then(tasks.step2).then(tasks.step1).catch(function(err) {
+//   console.log(err)
+// })
