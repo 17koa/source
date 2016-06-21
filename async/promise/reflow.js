6ddea9bd83d66acfb7485cb5e94c4d9f7f302448
i5ting <i5ting@126.com>
2016-06-21 23:26:27 +0800

    lazycommmit, please ignore

---
+++ async/promise/reflow.js
@@ -0,0 +1,58 @@
+// callbacks to promise
+var fs = require("fs");
+
+function hello (file) {
+  return new Promise(function(resolve, reject){
+    fs.readFile(file, (err, data) => {
+    	if (err) {
+    		reject(err);
+    	} else {
+    		resolve(data.toString())
+    	}
+    });
+  });
+}
+
+
+
+hello('./package.json').then(function(data){
+  console.log('way 1:\n')
+  return new Promise(function(resolve, reject){
+    console.log('promise result = ' + data)
+    resolve(data)
+  });
+}).then(function(data){
+  return new Promise(function(resolve, reject){
+    resolve('1')
+  });
+}).then(function(data){
+  console.log(data)
+  
+  return new Promise(function(resolve, reject){
+    reject(new Error('reject with custom err'))
+  });
+}).catch(function(err) {
+  console.log(err)
+})
+
+
+
+hello('./package.json').then(function(data){
+  console.log('\n\nway 2:\n')
+  return new Promise(function(resolve, reject){
+    console.log('promise result = ' + data)
+    resolve(data)
+  }).then(function(data){
+    return new Promise(function(resolve, reject){
+      resolve('1')
+    });
+  })
+}).then(function(data){
+  console.log(data)
+  
+  return new Promise(function(resolve, reject){
+    reject(new Error('reject with custom err'))
+  });
+}).catch(function(err) {
+  console.log(err)
+})
