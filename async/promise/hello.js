816c4da0d0f9aa5152addfe79af62830f0250eb8
i5ting <i5ting@126.com>
2016-06-21 21:10:13 +0800

    lazycommmit, please ignore

---
+++ async/promise/hello.js
@@ -0,0 +1,7 @@
+// callbacks
+var fs = require("fs");
+
+fs.readFile('./package.json', (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
---
+++ async/promise/hellopromise.js
@@ -0,0 +1,20 @@
+// callbacks to promise
+var fs = require("fs");
+
+function hello (file) {
+  return new Promise(function(resolve, reject){
+    fs.readFile(file, (err, data) => {
+    	if (err) {
+    		reject(err);
+    	} else {
+    		resolve(data.toString())
+    	}
+    });
+  });
+}
+
+hello('./package.json').then(function(data){
+  console.log('promise result = ' + data)
+}).catch(function(err) {
+  console.log(err)
+})
