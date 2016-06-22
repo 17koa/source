091401bd730583fa3041272b011c5a97d9e31d84
i5ting <i5ting@126.com>
2016-06-22 16:02:40 +0800

    lazycommmit, please ignore

---
+++ async/promise/api/catch3.js
@@ -0,0 +1,17 @@
+var p1 = new Promise(function(resolve, reject) {
+  resolve('Success');
+});
+
+p1.then(function(value) {
+  console.log(value); // "Success!"
+  return Promise.reject('oh, no!');
+}).catch(function(e) {
+  console.log(e); // "oh, no!"
+  return Promise.reject('oh, no! 2');
+}).then(function(){
+  console.log('after a catch the chain is restored');
+}, function () {
+  console.log('Not fired due to the catch');
+}).catch(function(e) {
+  console.log('catch ....'); // "oh, no!"
+})
