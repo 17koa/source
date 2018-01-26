eb06d663664495b41563a39960b642a1bc993132
狼叔 <langshu.ssl@alibaba-inc.com>
2018-01-26 21:12:42 +0800

    lazycommmit, please ignore

--- db/mini/connect.js
+++ db/mini/connect.js
@@ -1,15 +1,28 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose")
+
+mongoose.Promise = require('bluebird')
 
 // 核心代码，是否开启测试
-mongoose.set('debug', false);
+mongoose.set('debug', true)
 
-var db = mongoose.connect("mongodb://127.0.0.1:27017/db_helloworld"); 
+const options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
 
-db.connection.on("error", function (error) {  
-  console.log("数据库连接失败：" + error); 
-}); 
+const db = mongoose.connect("mongodb://127.0.0.1:27017/db_helloworld", options) 
 
-db.connection.on("open", function () {  
-  console.log("数据库连接成功");
-  mongoose.connection.db.dropDatabase();
-});
+// db.connection.on("error", function (error) {  
+//   console.log("数据库连接失败：" + error) 
+// }) 
+
+// db.connection.once('open', function() {
+//   // we're connected!
+//   console.log("数据库连接成功")
+//   mongoose.connection.db.dropDatabase()
+// });
